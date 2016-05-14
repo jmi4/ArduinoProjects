edf6019c976bc53e7843a2744c8da7a6a49d90c5
Jeremy Miller <jeremy.j.miller@cerner.com>
2016-05-13 21:35:53 -0500

    Uploading fritzing sketch and updated code

--- test_Hello_Ella/test_Hello_Ella.ino
+++ test_Hello_Ella/test_Hello_Ella.ino
@@ -22,20 +22,30 @@
  * wiper to LCD VO pin (pin 3)
 
  */
-
 // include the library code:
 #include <LiquidCrystal.h>
+
+// Button setup
+const int  buttonPin = 6;
+
+// Variables will change:
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
+  // initialize the button pin as a input:
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print greeting message to the LCD.
   lcd.setCursor(2, 0);
   lcd.print("Hello Ella!");
-  delay(5000);
+  delay(3000);
   lcd.setCursor(0, 1);
   lcd.print("Want Treats?");
   delay(3000);
@@ -50,7 +60,30 @@ void loop() {
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+  // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
+
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button
+      // went from off to on:
+      buttonPushCounter++;
+      Serial.println("on");
+      lcd.clear();
+      lcd.print(buttonPushCounter);
+    } else {
+      // if the current state is LOW then the button
+      // wend from on to off:
+      Serial.println("off");
+    }
+    // Delay a little bit to avoid bouncing
+    delay(50);
+  }
+  // save the current state as the last state,
+  //for next time through the loop
+  lastButtonState = buttonState;
+
 }
 
